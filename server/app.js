8faeed95f6adba79888edbefc3a2d7fdf9488f94
Jeremy Neo <doremineo@gmail.com>
2021-12-22 16:27:07 +0000

    change client side app.js to use react router

--- server/app.js
+++ server/app.js
@@ -18,10 +18,4 @@ app.use(
     })
 );
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port)
