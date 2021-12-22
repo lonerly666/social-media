eac6f22b5f662fbfb82f881aa957b4fd190085a3
Jeremy Neo <doremineo@gmail.com>
2021-12-22 13:27:36 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# social-media app
+
+This app is inspired by Facebook and it is a side-project of mine
---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const CLIENT_URL = "http://localhost:3000";
+const cors = require("cors");
+const app = express();
+const port = 5000;
+
+app.use(express.json());
+
+app.use(express.urlencoded({
+  extended: true
+}));
+
+app.use(
+    cors({
+      origin: CLIENT_URL,
+      methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+      credentials: true
+    })
+);
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
