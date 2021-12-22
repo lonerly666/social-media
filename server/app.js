7be8d65b176362dc3ccdea930b96de7dc93ea8e8
Jeremy Neo <doremineo@gmail.com>
2021-12-22 20:26:21 +0000

    created user model

--- server/app.js
+++ server/app.js
@@ -1,15 +1,21 @@
 const express = require('express');
+require('dotenv').config({path: __dirname + '/./.env'});
 const CLIENT_URL = "http://localhost:3000";
 const cors = require("cors");
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
+const inProduction = process.env.NODE_ENV === "production";
+const CLIENT_URL = inProduction ? process.env.DOMAIN_NAME : "http://localhost:3000";
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const mongoURI = "mongodb://localhost:27017/social-media"
+const passport = require('passport');
 
 app.use(express.json());
-
 app.use(express.urlencoded({
   extended: true
 }));
-
 app.use(
     cors({
       origin: CLIENT_URL,
@@ -17,5 +23,17 @@ app.use(
       credentials: true
     })
 );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+mongoose.connect(mongoURI,{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+});
+mongoose.set("useCreateIndex", true);
+mongoose.set('useFindAndModify', false);
+
 
 app.listen(port)
---
+++ server/models/userModel.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const findOrCreate = require('mongoose-findorcreate');
+const passportLocalMongoose = require('passport-local-mongoose');
+const { ObjectId } = require('mongodb');
+
+const userSchema = new mongoose.Schema({
+    username:String,
+    password:String,
+    googleId:String,
+    facebookId:String,
+    nickname:String,
+    dateOfBirth:Date,
+    bio:String,
+    gender:String,
+    friendList:[ObjectId],
+    numOfPosts:{type:Number,default:0},
+    lastSeenNotification:{type:Date,default:new Date(0)},
+    lastSeenFriendRequest:{type:Date,default:new Date(0)}
+})
+
+userSchema.plugin(passportLocalMongoose)
+userSchema.plugin(findOrCreate)
+
+const User = new mongoose.model("User",userSchema)
+
+module.exports = User
